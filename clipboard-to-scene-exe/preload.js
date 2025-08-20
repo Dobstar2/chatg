9a295fc97a8d4c0397cca654568603bdc171683c
Dobstar2 <128965154+Dobstar2@users.noreply.github.com>
2025-08-20 12:30:02 +0100

    Delete clipboard-to-scene-exe directory

--- clipboard-to-scene-exe/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('appApi', {
-  readClipboard: () => ipcRenderer.invoke('read-clipboard'),
-  saveScene: (obj) => ipcRenderer.invoke('save-scene', obj),
-  exportSceneAs: (obj) => ipcRenderer.invoke('export-scene-as', obj)
-});
