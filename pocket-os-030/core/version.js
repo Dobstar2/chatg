d3e5022ac8296ed3aae281ba13dc4a8bea1ad9b5
Dobstar2 <128965154+Dobstar2@users.noreply.github.com>
2026-08-12 10:04:23 +0100

    Version Pocket OS pictogram icon update

--- pocket-os-030/core/version.js
+++ pocket-os-030/core/version.js
@@ -1,2 +1,2 @@
-export const BUILD = 'pocket-spatial-os-0.3.0-candidate.1-handfix.1';
-export const BUILD_SHORT = 'POCKET OS 0.3 C1-H1';
+export const BUILD = 'pocket-spatial-os-0.3.0-candidate.1-handfix.1-icons.1';
+export const BUILD_SHORT = 'POCKET OS 0.3 C1-H1-I1';
